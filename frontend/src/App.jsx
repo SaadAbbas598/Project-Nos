f3fe71658eaf0c81a29aba7dde53e6e16c06933c
Saad Abbas <2023cs598@student.uet.edu.pk>
2025-10-25 00:44:37 +0500

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,37 +1,37 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-// Auth pages
-import Login from "./auth/login";
-import Signup from "./auth/signup";
+// Auth pages (corrected case)
+import Login from "./auth/Login";
+import Signup from "./auth/Signup";
 
 // Main components
 import Navbar from "./components/Navbar";
 import Dashboard from "./Pages/Dashboard";
 import About from "./Pages/About";
 import Section from "./Pages/Section";
-import PlatformSection from "./Pages/Platefoam";
+import PlatformSection from "./Pages/PlatformSection"; // also corrected filename spelling
 import Footer from "./Pages/Footer";
 
 function App() {
   return (
     <Router>
-      {/* Navbar should  appear on all pages except login/signup */}
       <Routes>
+        {/* Home route */}
         <Route
           path="/"
           element={
             <>
               <Navbar />
-              <Dashboard/>
+              <Dashboard />
               <About />
               <Section />
               <PlatformSection />
               <Footer />
-              
             </>
           }
         />
+        {/* Auth routes */}
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
       </Routes>
